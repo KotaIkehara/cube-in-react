de967ecea04fbb05dd454181cf2601c7a7efb3f9
KotaIkehara <ikehara.aurora95@gmail.com>
2022-02-18 15:31:36 +0900

    Add fiber and drei

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,29 @@
-// import logo from './logo.svg';
-// import './App.css';
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera, TransformControls } from "@react-three/drei";
 
-function App() {
-  return (
-    <div className="App">
-    Hello
+
+export default function App() {
+     return (
+    <div className="App" id="root">
+      {/* start to define react three fiber scene */}
+      <Canvas>
+        <PerspectiveCamera/>
+        {/* makeDefault will be desabled automatically when the user is pulling the transfomr gizmo */}
+        <OrbitControls makeDefault/>
+
+        <pointLight position={[10, 10, 10]} />
+        <ambientLight intensity={0.50} />
+        <gridHelper visible={true} args={[10, 10]} />
+
+        <TransformControls mode="translate">
+        <mesh position={[0,0.5,0]}>
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial attach='material' />
+        </mesh>
+        </TransformControls>
+
+      </Canvas>
     </div>
   );
 }
-
-export default App;
